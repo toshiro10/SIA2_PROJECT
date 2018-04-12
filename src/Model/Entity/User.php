1f4653c2804661430c791c16d4b9f4cd91a6c595
js-dev88 <js.dev88@gmail.com>
2018-04-13 01:03:57 +0200

    password encryption in registration

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -1,6 +1,6 @@
 <?php
 namespace App\Model\Entity;
-
+use Cake\Auth\DefaultPasswordHasher; 
 use Cake\ORM\Entity;
 
 /**
@@ -43,4 +43,14 @@ class User extends Entity
     protected $_hidden = [
         'password'
     ];
+
+     // Password Encryption
+    protected function _setPassword($value)
+    {
+        if (strlen($value)) {
+            $hasher = new DefaultPasswordHasher();
+
+            return $hasher->hash($value);
+        }
+    }
 }
