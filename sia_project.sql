0702966148d78e779fe06099d0543f563ed3f141
Francisco Javier Martinez Lago <edoreld@gmail.com>
2018-06-12 10:52:17 +0200

    changes to book table to allow for file uploads

--- sia_project.sql
+++ sia_project.sql
@@ -1,152 +1,27 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.7.7
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  mar. 15 mai 2018 à 07:42
--- Version du serveur :  5.7.21
--- Version de PHP :  7.2.4
+-- Host: localhost:8889
+-- Generation Time: Jun 12, 2018 at 10:51 AM
+-- Server version: 5.6.38
+-- PHP Version: 7.2.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
--- Base de données :  `sia_project`
+-- Database: `Research`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `authors_books`
+-- Table structure for table `articles`
 --
 
-DROP TABLE IF EXISTS `authors_books`;
-CREATE TABLE IF NOT EXISTS `authors_books` (
-  `id_author` int(11) NOT NULL,
-  `id_book` int(11) NOT NULL,
-  PRIMARY KEY (`id_author`,`id_book`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-COMMIT;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `authors`
---
-
-DROP TABLE IF EXISTS `authors`;
-CREATE TABLE IF NOT EXISTS `authors` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_user` int(50) DEFAULT NULL,
-  `authorname` varchar(50) DEFAULT NULL,
-  `authorfirstname` varchar(50) DEFAULT NULL,
-  `authorfullname` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-
-
--- --------------------------------------------------------
-
---
--- Structure de la table `books`
---
-
-DROP TABLE IF EXISTS `books`;
-CREATE TABLE IF NOT EXISTS `books` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `modified` datetime NOT NULL,
-  `created` datetime,
-  `year` varchar(255),
-  `title` varchar(255),
-  `editor_id` int(11),
-  `isbn` varchar(255),
-  `ee` varchar(255),
-  `series` varchar(255),
-  `url` varchar(255),
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `incollections`
---
-
-DROP TABLE IF EXISTS `incollections`;
-CREATE TABLE IF NOT EXISTS `incollections` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `mdate` date DEFAULT NULL,
-  `modified` datetime DEFAULT NULL,
-  `created` datetime DEFAULT CURRENT_TIMESTAMP,
-  `year` int(11) DEFAULT NULL,
-  `publication_key` varchar(255) DEFAULT NULL,
-  `title` varchar(255) NOT NULL,
-  `pages` varchar(50) DEFAULT NULL,
-  `crossref` varchar(255) DEFAULT NULL,
-  `title_book` varchar(255) DEFAULT NULL,
-  `ee` varchar(255) DEFAULT NULL,
-  `url` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-COMMIT;
-
---
--- Structure de la table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `role` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `created` datetime DEFAULT NULL,
-  `modified` datetime DEFAULT NULL,
-  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `firstname` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `lastname` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `picture` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Déchargement des données de la table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `password`, `role`, `created`, `modified`, `email`, `firstname`, `lastname`, `picture`) VALUES
-(1, 'a', '$2y$10$0TWYE5o1Do9puaJ2IUVBweYrFeLo3GVUvgLawyqTXkdlYTDLQ7lWq', 'admin', '2018-05-09 11:55:32', '2018-05-09 11:55:32', 'a@a', 'a', 'a', NULL),
-(2, 'b', '$2y$10$0dU4NfckAI4ynCjzCccSi.GJy1wksY4TQ.fIVv3WCi1BDtPb.cSeu', 'admin', '2018-05-09 12:26:11', '2018-05-09 12:26:11', 'b@b', 'b', 'b', NULL);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-
-
--- --------------------------------------------------------
-
---
--- Structure de la table `articles`
---
-
-DROP TABLE IF EXISTS `articles`;
-CREATE TABLE IF NOT EXISTS `articles` (
-  `id` int(50) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `articles` (
+  `id` int(50) NOT NULL,
   `id_book` int(50) DEFAULT NULL,
   `id_state` int(50) NOT NULL DEFAULT '1',
   `mdate` date NOT NULL,
@@ -159,81 +34,262 @@ CREATE TABLE IF NOT EXISTS `articles` (
   `url` varchar(255) DEFAULT NULL,
   `ee` varchar(255) DEFAULT NULL,
   `pages` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `photo` varchar(255) DEFAULT NULL,
+  `photo_dir` varchar(255) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- Dumping data for table `articles`
+--
 
+INSERT INTO `articles` (`id`, `id_book`, `id_state`, `mdate`, `lkey`, `title`, `year`, `volume`, `journal`, `number`, `url`, `ee`, `pages`, `photo`, `photo_dir`) VALUES
+(1, 100, 1, '2018-06-12', 'kk', 'kk', 'kk', 123, 'kk', 123, 'kk', 'kk', 'kk', NULL, NULL),
+(2, 2, 1, '2018-06-12', 'ooo', 'oo', '1928', 1, 'o', 1, 'o', 'o', 'o', NULL, NULL);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `states`
+-- Table structure for table `authorbooks`
 --
 
-DROP TABLE IF EXISTS `states`;
-CREATE TABLE IF NOT EXISTS `states` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `state` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+CREATE TABLE `authorbooks` (
+  `idAuthors` int(11) NOT NULL,
+  `idBooks` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
--- Déchargement des données de la table `states`
+-- Table structure for table `authors`
+--
+
+CREATE TABLE `authors` (
+  `id` int(11) NOT NULL,
+  `id_user` int(50) DEFAULT NULL,
+  `authorname` varchar(50) DEFAULT NULL,
+  `authorfirstname` varchar(50) DEFAULT NULL,
+  `authorfullname` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `authors_articles`
+--
+
+CREATE TABLE `authors_articles` (
+  `id_article` int(50) NOT NULL,
+  `id_author` int(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `id` int(11) NOT NULL,
+  `modified` datetime NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `year` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `editor_id` int(11) DEFAULT NULL,
+  `isbn` varchar(255) DEFAULT NULL,
+  `ee` varchar(255) DEFAULT NULL,
+  `series` varchar(255) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `photo` varchar(255) DEFAULT NULL,
+  `photo_dir` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`id`, `modified`, `created`, `year`, `title`, `editor_id`, `isbn`, `ee`, `series`, `url`, `photo`, `photo_dir`) VALUES
+(1, '2018-06-12 07:47:21', '2018-06-12 07:47:21', '2000', 'kk', NULL, 'kk', 'kk', 'kk', 'kk', 'Screen Shot 2018-06-12 at 09.26.46.png', '9e35e54f-7f91-41cb-a73f-d9ee83df3ce1'),
+(2, '2018-06-12 08:29:48', '2018-06-12 08:29:48', '3000', 'qqq', NULL, 'qqq', 'qqq', 'qqq', 'qqq', 'Screen Shot 2018-06-05 at 13.13.46.png', 'a8dce8b3-51d9-44bd-b689-45b848e77df8');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `incollections`
+--
+
+CREATE TABLE `incollections` (
+  `id` int(11) NOT NULL,
+  `mdate` date NOT NULL,
+  `modified` datetime NOT NULL,
+  `created` datetime NOT NULL,
+  `year` int(11) NOT NULL,
+  `publication_key` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `pages` varchar(50) NOT NULL,
+  `crossref` varchar(255) DEFAULT NULL,
+  `title_book` varchar(255) DEFAULT NULL,
+  `ee` varchar(255) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `states`
+--
+
+CREATE TABLE `states` (
+  `id` int(11) NOT NULL,
+  `state` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `states`
 --
 
 INSERT INTO `states` (`id`, `state`) VALUES
 (1, 'Brouillon'),
 (2, 'Publié');
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `authors_articles`
+-- Table structure for table `teams`
 --
 
-DROP TABLE IF EXISTS `authors_articles`;
-CREATE TABLE IF NOT EXISTS `authors_articles` (
-  `id_article` int(50) NOT NULL,
-  `id_author` int(50) NOT NULL,
-  PRIMARY KEY (`id_article`,`id_author`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE `teams` (
+  `id` int(11) NOT NULL,
+  `photo` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `photo_dir` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `inproceedings`
+-- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `inproceedings`;
-CREATE TABLE IF NOT EXISTS `inproceedings` (
-  `id` int(50) NOT NULL AUTO_INCREMENT,
-  `id_book` int(50) DEFAULT NULL,
-  `id_state` int(50) NOT NULL DEFAULT '1',
-  `mdate` date NOT NULL,
-  `lkey` varchar(255) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `year` varchar(255) DEFAULT NULL,
-  `crossref` varchar(255) DEFAULT NULL,
-  `booktitle` varchar(255) DEFAULT NULL,
-  `url` varchar(255) DEFAULT NULL,
-  `ee` varchar(255) DEFAULT NULL,
-  `pages` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=359 DEFAULT CHARSET=utf8;
-COMMIT;
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `username` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `role` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `firstname` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `lastname` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `picture` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `role`, `created`, `modified`, `email`, `firstname`, `lastname`, `picture`) VALUES
+(1, 'a', '$2y$10$0TWYE5o1Do9puaJ2IUVBweYrFeLo3GVUvgLawyqTXkdlYTDLQ7lWq', 'admin', '2018-05-09 11:55:32', '2018-05-09 11:55:32', 'a@a', 'a', 'a', NULL),
+(2, 'b', '$2y$10$0dU4NfckAI4ynCjzCccSi.GJy1wksY4TQ.fIVv3WCi1BDtPb.cSeu', 'admin', '2018-05-09 12:26:11', '2018-05-09 12:26:11', 'b@b', 'b', 'b', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `articles`
+--
+ALTER TABLE `articles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `authorbooks`
+--
+ALTER TABLE `authorbooks`
+  ADD PRIMARY KEY (`idAuthors`,`idBooks`);
+
+--
+-- Indexes for table `authors`
+--
+ALTER TABLE `authors`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `authors_articles`
+--
+ALTER TABLE `authors_articles`
+  ADD PRIMARY KEY (`id_article`,`id_author`);
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `incollections`
+--
+ALTER TABLE `incollections`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `states`
+--
+ALTER TABLE `states`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `teams`
+--
+ALTER TABLE `teams`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `articles`
+--
+ALTER TABLE `articles`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `authors`
+--
+ALTER TABLE `authors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `incollections`
+--
+ALTER TABLE `incollections`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `states`
+--
+ALTER TABLE `states`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `teams`
+--
+ALTER TABLE `teams`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
